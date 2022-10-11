095ab15cfd166180a81c4da98c5ef9e6382b702a
Yahya <yahyahussein670@gmail.com>
2022-10-11 22:53:21 +0300

    Add the relationships between the tables, plus validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ApplicationRecord
+    belongs_to :users
+
+    CATEGORY = ["Birthday", "Book club", "Graduation party", "pool party" ]
+
+    validates :category, inclusion: { 
+        in: CATEGORY
+        message: "The category must be one of the following: #{CATEGORY.join(',')}"
+     }
+
+     validates :image_url, presence: true
+     validates :description, length: { minimum: 5 }
 end
