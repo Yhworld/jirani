10a4be7ba8df75ecf26d4a8b8d451c4a72ee67dd
Yahya <yahyahussein670@gmail.com>
2022-10-12 23:11:55 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
