bcd5c20cdb40d1115c339049a6807d282e75b7de
Yahya <yahyahussein670@gmail.com>
2022-10-14 09:26:09 +0300

    Add new routes, an add event route, add post route, a get post route and a get events route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
+  post "/addevent", to: "events#create"
+  post "/addpost", to: "posts#create"
+  get "posts", to: "posts#index"
+  get "/events", to: "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
