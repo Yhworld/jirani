34987011648fcdbd44729b0cb2fe80599fa7cc11
Yahya <yahyahussein670@gmail.com>
2022-10-15 17:01:59 +0300

    updated the event model by removing the validations in place

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ApplicationRecord
-    belongs_to :users
+    belongs_to :user
 
-    CATEGORY = ["Birthday", "Book club", "Graduation party", "pool party" ]
+    # CATEGORY = ["Birthday", "Book club", "Board games", "Graduation party", "pool party" ]
 
-    validates :category, inclusion: { 
-        in: CATEGORY
-        message: "The category must be one of the following: #{CATEGORY.join(',')}"
-     }
+    # validates :category, inclusion: {
+    #     in: CATEGORIES,
+    #     message: "must be one of the following: #{CATEGORIES.join(',')}"
+    # }
 
      validates :image_url, presence: true
      validates :description, length: { minimum: 5 }
