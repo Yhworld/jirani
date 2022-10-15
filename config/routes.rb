2eaeb81524d7d50b567e45cce66d2fc4b4b10cf8
Yahya <yahyahussein670@gmail.com>
2022-10-15 17:00:37 +0300

    Add an update profile, logout and likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post "/addpost", to: "posts#create"
   get "posts", to: "posts#index"
   get "/events", to: "events#index"
+  patch "/updateprofile", to: "users#update"
+  delete "/logout", to: "sessions#destroy"
+  patch "/likes", to: "posts#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
