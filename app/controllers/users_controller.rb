c393f721535792ded7a658858b527b743c5e975f
Yahya <yahyahussein670@gmail.com>
2022-10-15 17:08:40 +0300

    Add the update method which enables users to update their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: :create
+    skip_before_action :authorize, only: [:create]
 
     def create
       user = User.create!(user_params)
@@ -10,10 +10,21 @@ class UsersController < ApplicationController
     def show
       render json: @current_user
     end
+
+    def update
+      if @current_user
+      @current_user.update!(update_params)
+      render json: @current_user, status: :accepted
+      end
+    end
   
     private
   
     def user_params
       params.permit(:username, :email, :password, :password_confirmation, :bio, :estate, :profile_pic, :followers, :following)
+    end
+
+    def update_params
+      params.permit(:username, :email, :profile_pic, :bio)
     end
 end
